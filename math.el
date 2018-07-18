c5b7f98489510854a21d282f358e22155d585358
Cole Lyman <cole@colelyman.com>
2018-07-18 12:18:41 -0600

    Fixed sum-to-n and added factorial

--- math.el
+++ math.el
@@ -9,7 +9,16 @@
   (let ((val (or val 0)))
     (if (= i n)
       val
-    (sum (1+ i) n (+ i val)))))
+    (sum-to-n (1+ i) n (+ i val)))))
+
+(defun factorial (n &optional i val)
+  "Return the factorial of N (I and VAL are used for the recursion).
+This is only useful for small numbers of N, because of overflow."
+  (let ((i (or i 1))
+        (val (or val 1)))
+    (if (> i n)
+        val
+      (factorial n (1+ i) (* i val)))))
 
 (provide 'math)
 ;;; math.el ends here
